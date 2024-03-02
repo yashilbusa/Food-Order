5e6b5c56e4cc23eac30e10cb12d13ca056625826
yashilbusa <yashilpatel1914@gmail.com>
2024-03-02 15:44:14 +0530

    c

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,8 +2,8 @@
 
 import React, { useState } from 'react'
 import { Link, useNavigate } from "react-router-dom";
-import Badge from "@material-ui/core/Badge";
-import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+// import Badge from "@material-ui/core/Badge";
+// import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
 import { useCart } from './ContextReducer';
 import Modal from '../Modal';
 import Cart from '../screens/Cart';
--- src/screens/Cart.js
+++ src/screens/Cart.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Delete from '@material-ui/icons/Delete'
+// import Delete from '@material-ui/icons/Delete'
 import { useCart, useDispatchCart } from '../components/ContextReducer';
 export default function Cart() {
   let data = useCart();
@@ -63,7 +63,8 @@ export default function Cart() {
                 <td>{food.qty}</td>
                 <td>{food.size}</td>
                 <td>{food.price}</td>
-                <td ><button type="button" className="btn p-0"><Delete onClick={() => { dispatch({ type: "REMOVE", index: index }) }} /></button> </td></tr>
+                {/* <td ><button type="button" className="btn p-0"><Delete onClick={() => { dispatch({ type: "REMOVE", index: index }) }} /></button> </td> */}
+              </tr>
             ))}
           </tbody>
         </table>
