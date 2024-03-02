e01d785fd318b862e8b4d5800f8bbc9517b8a454
yashilbusa <yashilpatel1914@gmail.com>
2024-03-02 15:55:16 +0530

    c

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ import { Link, useNavigate } from "react-router-dom";
 // import Badge from "@material-ui/core/Badge";
 // import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
 import { useCart } from './ContextReducer';
-import Modal from '../Modal';
+// import Modal from '../Modal';
 import Cart from '../screens/Cart';
 export default function Navbar(props) {
 
@@ -26,9 +26,11 @@ export default function Navbar(props) {
     return (
         <div>
             <nav className="navbar navbar-expand-lg navbar-dark bg-success position-sticky"
-                style={{ boxShadow: "0px 10px 20px black", filter: 'blur(20)', position: "fixed", zIndex: "10", width: "100%" }}>
+                // style={{ boxShadow: "0px 10px 20px black", filter: 'blur(20)', position: "fixed", zIndex: "10", width: "100%" }}
+                >
+                
                 <div className="container-fluid">
-                    <Link className="navbar-brand fs-1 fst-italic" to="/">GoFood</Link>
+                    <Link className="navbar-brand fs-1 fst-italic" to="/">FoodOrder</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
@@ -56,7 +58,7 @@ export default function Navbar(props) {
                                     Cart
                                 </div>
 
-                                {cartView ? <Modal onClose={() => setCartView(false)}><Cart></Cart></Modal> : ""}
+                                {/* {cartView ? <Modal onClose={() => setCartView(false)}><Cart></Cart></Modal> : ""} */}
 
                                 <button onClick={handleLogout} className="btn bg-white text-success" >Logout</button></div>}
                     </div>
